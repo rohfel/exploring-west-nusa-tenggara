f143226c8ab2ee14ec60fd44cee4fcb8c6cc93bb
Rohfel Adyaraka Christianugrah Puspoasmoro <rohfeladyaraka@gmail.com>
2022-12-13 08:26:18 +0700

    added a basic sveltekit project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,41 @@
+<nav class="flex justify-center">
+    <a class="mx-20 mt-5">Home</a>
+    <a class="mx-20 mt-5">Explore</a>
+    <a class="mx-20 mt-5">About</a>
+</nav>
+
+<header>
+    <p class="text-9xl text-center font-bold mt-20">Exploring</p>
+    <p class="text-7xl text-center font-bold mt-5 text-red-500">West Nusa Tenggara</p>
+    <p class="text-lg text-center mt-5">A website for you to explore West Nusa Tenggara!</p>
+
+    <div class="flex mt-5 justify-center">
+        <button class="p-3 rounded-md text-white bg-red-500">
+            Let's Explore!
+        </button>
+    </div>
+</header>
+
+<main>
+    <div class="m-10 text-center">
+        <p class="font-bold text-5xl text-red-500">A province of Indonesia</p>
+        <p class="my-5 text-xl">It has an enormous amount of cultures, foods, and natural resources</p>
+    </div>
+
+    <div class="flex flex-col justify-center m-10">
+        <div class="grid grid-cols-2 my-10">
+            <div>
+                <p class="font-bold text-3xl text-red-500">See the enormous cultures that will amaze you</p>
+                <button class="my-5 p-3 rounded-md text-white bg-red-500">See the cultures</button>
+            </div>
+            <p class="text-lg">West Nusa Tenggara is full of amazing and unique cultures</p>
+        </div>
+        <div class="grid grid-cols-2 my-10">
+            <p class="text-lg">West Nusa Tenggara has a wide amount of natural resources</p>
+            <div>
+                <p class="font-bold text-3xl text-red-500">See the wide amount of natural resources</p>
+                <button class="my-5 p-3 rounded-md text-white bg-red-500">See the natural resources</button>
+            </div>
+        </div>
+    </div>
+</main>
